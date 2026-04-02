71fabd9bfb1bc1cb6caa23bf7a368cd8e88a6a57
Walker Lee <walker8088@gmail.com>
2026-04-02 20:58:59 +0800

    bug fix

--- run_test.bat
+++ run_test.bat
@@ -1,3 +1,3 @@
 uvx ruff check ./src
-uv run pytest -v --tb=short --cov=src --show-capture=no --capture=no  .\tests\
+uv run python -m pytest -v --tb=short --cov=src --show-capture=no --capture=no  .\tests\
 pause
